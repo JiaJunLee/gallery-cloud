a8dd78f3f6990588671635ca2815fd142db448c7
libu <burgess.li@oocl.com>
2019-03-24 21:34:59 +0800

    fix user controller demo bug

--- gallery-user-service/src/main/java/com/oocl/ita/gallery/user/service/user/UserController.java
+++ gallery-user-service/src/main/java/com/oocl/ita/gallery/user/service/user/UserController.java
@@ -4,9 +4,11 @@ import com.oocl.ita.gallery.common.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/users")
+@RestController
+@RequestMapping
 public class UserController {
 
   @GetMapping
