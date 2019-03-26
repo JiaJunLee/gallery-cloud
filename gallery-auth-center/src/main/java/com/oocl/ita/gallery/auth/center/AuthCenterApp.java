6e6e2d5d158e6d6fc16e1ffdaa71574dad65f4f3
CAIKE3 <kevin.cai@oocl.com>
2019-03-26 11:25:21 +0800

    update auth center 2.0

--- gallery-auth-center/src/main/java/com/oocl/ita/gallery/auth/center/AuthCenterApp.java
+++ gallery-auth-center/src/main/java/com/oocl/ita/gallery/auth/center/AuthCenterApp.java
@@ -2,7 +2,6 @@ package com.oocl.ita.gallery.auth.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
@@ -12,6 +11,6 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 public class AuthCenterApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthCenterApp.class,args);
+        SpringApplication.run(AuthCenterApp.class, args);
     }
 }
