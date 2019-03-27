2fd151cad5b5668e56d0bc60a81a9fc0607a8a99
CAIKE3 <kevin.cai@oocl.com>
2019-03-27 09:16:06 +0800

    security 4.0

--- gallery-gateway/src/main/java/com/oocl/ita/gallery/gateway/security/config/GalleryFilterSecurityInterceptor.java
+++ gallery-gateway/src/main/java/com/oocl/ita/gallery/gateway/security/config/GalleryFilterSecurityInterceptor.java
@@ -7,7 +7,6 @@ import org.springframework.security.access.intercept.InterceptorStatusToken;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import javax.servlet.*;
 import java.io.IOException;
@@ -36,10 +35,10 @@ public class GalleryFilterSecurityInterceptor extends AbstractSecurityIntercepto
         invoke(filterInvocation);
     }
 
-    public void invoke(FilterInvocation fi) throws IOException, ServletException {
-        InterceptorStatusToken token = super.beforeInvocation(fi);
+    public void invoke(FilterInvocation filterInvocation) throws IOException, ServletException {
+        InterceptorStatusToken token = super.beforeInvocation(filterInvocation);
         try {
-            fi.getChain().doFilter(fi.getRequest(), fi.getResponse());
+            filterInvocation.getChain().doFilter(filterInvocation.getRequest(), filterInvocation.getResponse());
         } finally {
             super.afterInvocation(token, null);
         }
