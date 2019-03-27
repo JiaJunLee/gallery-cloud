40d1f5ce37826ff8727f63952023f4f8d076fbbe
CAIKE3 <kevin.cai@oocl.com>
2019-03-27 19:45:10 +0800

    update config

--- gallert-role-service/src/main/java/com/oocl/ita/gallery/role/service/role/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oocl.ita.gallery.role.service.role;
-
-import com.oocl.ita.gallery.common.model.Role;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface RoleRepository extends PagingAndSortingRepository<Role, String> {
-    Role findByName(String name);
-}
