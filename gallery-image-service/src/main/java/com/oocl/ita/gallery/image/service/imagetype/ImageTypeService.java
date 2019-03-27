723b99519f113065ec0dbef38c5ba1490409c976
HELLEN HAN <HELLEN.HAN@OOCL.COM>
2019-03-27 20:25:30 +0800

    fix imagetype

--- gallery-image-service/src/main/java/com/oocl/ita/gallery/image/service/imagetype/ImageTypeService.java
+++ gallery-image-service/src/main/java/com/oocl/ita/gallery/image/service/imagetype/ImageTypeService.java
@@ -4,9 +4,9 @@ import com.oocl.ita.gallery.common.model.BaseService;
 import com.oocl.ita.gallery.common.model.ImageType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-@Repository
+@Service
 public class ImageTypeService extends BaseService<ImageType, String> {
 
   @Autowired
