681e69aeabd0d50ade15c58424aed5f12d90c194
lance <minelhjun@gmail.com>
2019-03-28 19:56:16 +0800

    update

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-mvn clean install -pl gallery-eureka,gallery-image-service,gallery-file-service,gallery-gateway,gallery-hystrix-dashboard,gallery-user-service docker:build
+mvn clean install -pl gallery-eureka,gallery-auth-center,gallery-role-service,gallery-image-service,gallery-file-service,gallery-gateway,gallery-hystrix-dashboard,gallery-user-service docker:build
